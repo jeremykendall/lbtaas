c4813ffb4c10e42901a26ff2bae6187c0323d5f3
Jeremy Kendall <jeremy@jeremykendall.net>
2013-11-02 13:38:38 -0500

    Removes twig

--- public/index.php
+++ public/index.php
@@ -17,17 +17,6 @@ $app->container->singleton('log', function () {
     return $log;
 });
 
-// Prepare view
-$app->view(new \Slim\Views\Twig());
-$app->view->parserOptions = array(
-    'charset' => 'utf-8',
-    'cache' => realpath('../templates/cache'),
-    'auto_reload' => true,
-    'strict_variables' => false,
-    'autoescape' => true
-);
-$app->view->parserExtensions = array(new \Slim\Views\TwigExtension());
-
 // Define routes
 $app->get('/', function () use ($app) {
 
